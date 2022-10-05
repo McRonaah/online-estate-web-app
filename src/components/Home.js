89ecf504f51e26e09e48c854c4762d71262f7d8f
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-05 17:15:14 +0300

    created Home Location and Page component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from  "react";
+import Location from "./Location";
+
+const Home = () => {
+    return (
+        <>
+         <Location />
+        </>
+    )
+}
+
+export default Home;

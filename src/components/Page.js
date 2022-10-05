55efafffbf90fb16357ad76332194487912a6e8e
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-05 17:18:39 +0300

    updated routers

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,4 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
+
+const Page = () => {
+    <Router>
+        <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        </Switch>
+    </Router>
+}
+
 export default Page;

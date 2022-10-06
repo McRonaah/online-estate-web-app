8db004b5573cd2866fd2828f4f97036b956ffc6a
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-06 14:07:38 +0300

    services info

---
+++ src/components/Services.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Services () {
+    return (
+        <div>
+            <h2>Payment</h2>
+                <p>A deposit of 30% to be made upon signing the offer letter. 
+                    Balance payable within 18 months.</p>
+            <h2>Update</h2>
+                <p>We will keep you updated with the progress of your unit, walk you through 
+                    crucial completion stages, and answer any questions that you may have.</p>
+            <h2>Handover</h2>
+                <p>Upon completion, the investor and developer will do a checklist of unit hand over and officially 
+                    hand it over upon signing hand over agreement.</p>
+            <h2>Management</h2>
+                <p>Blue Sky oes the work for you! We look for tenants, collect rent and manage the unit on your behalf.
+                     Yours is to monitor your investment.</p>
+        </div>
+    );
+}
+
+export default Services;

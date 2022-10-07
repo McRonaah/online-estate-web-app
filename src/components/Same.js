03506887381acc566b9de290dd3c5b81ef2bd176
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-07 12:43:11 +0300

    cards styling

--- src/components/Same.js
+++ src/components/Same.js
@@ -31,25 +31,6 @@ const Same = () => {
                     Read More
                 </NavLink>
                 </div>
-
-
-
-
-                {/* <div>
-<hi>Our Services</hi>
-<h2>Payment</h2>
-    <p>A deposit of 30% to be made upon signing the offer letter. 
-        Balance payable within 18 months.</p>
-<h2>Update</h2>
-    <p>We will keep you updated with the progress of your unit, walk you through 
-        crucial completion stages, and answer any questions that you may have.</p>
-<h2>Handover</h2>
-    <p>Upon completion, the investor and developer will do a checklist of unit hand over and officially 
-        hand it over upon signing hand over agreement.</p>
-<h2>Management</h2>
-    <p>Blue Sky oes the work for you! We look for tenants, collect rent and manage the unit on your behalf.
-         Yours is to monitor your investment.</p>
-</div> */}
             </div>
            </div>
         </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -7,14 +7,14 @@ import manageimg from "./images/manage.jpg";
 
 function Services () {
     return (
-      <div>
+      <div className="serviceBody">
         <h1>Our Services</h1>
         <div className="services">
             <div className="card">
                 <h2>Payment</h2>
-                <img src={paymentimg} alt="payment" />
+                <img src={paymentimg} alt="payment" className="serviceImg" />
                 <p>A deposit of 30% to be made upon signing the offer letter. 
-                    Balance payable within 18 months.</p>
+                    Balance payable in installments within 18 months.</p>
                 <div className="btnBox">
                     <div className="btn">
                     <NavLink to='/components/About' className="readMore" activeStyle>
@@ -25,7 +25,7 @@ function Services () {
             </div>
             <div className="card">
                 <h2>Update</h2>
-                <img src={updateimg} alt="payment" />
+                <img src={updateimg} alt="payment" className="serviceImg"/>
                 <p>We will keep you updated with the progress of your unit, walk you through 
                     crucial completion stages, and answer any questions that you may have.</p>
                 <div className="btnBox">
@@ -38,7 +38,7 @@ function Services () {
             </div>
             <div className="card">
                 <h2>Handover</h2>
-                <img src={handoverimg} alt="payment" />
+                <img src={handoverimg} alt="payment" className="serviceImg"/>
                 <p>Upon completion, the investor and developer will do a checklist of unit hand over and officially 
                     hand it over upon signing hand over agreement.</p>
                 <div className="btnBox">
@@ -51,7 +51,7 @@ function Services () {
             </div>
             <div className="card">
                 <h2>Management</h2>
-                <img src={manageimg} alt="payment" />
+                <img src={manageimg} alt="payment" className="serviceImg"/>
                 <p>Blue Skies does the work for you! We look for tenants, collect rent and manage the unit on your behalf.
                     Yours is to monitor your investment.</p>
                 <div className="btnBox">

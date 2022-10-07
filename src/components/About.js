fd40f875a0689cb55f33ee5cacf4f2d3690208d4
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-07 07:57:50 +0300

    updated my about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,9 @@ function About() {
                 <h2>Know More About US</h2>
                 <p>With Broad experience stretching over a decade, East Africa Based Blue Skies Group 
                     has a diverse interest in Manufacturing, Trading, Construction, and Real Estate</p>
-                <p></p>
+                <p>Today the Group Operates a Diverse range of businesses including manufacturing floor 
+                    and wall tiles, Manufacturing bituminous products and trading, supplying and fitting of 
+                    buildings, road construction materials, and investments in Real estate.</p>
             </div>
         </div>
     );

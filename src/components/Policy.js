8dbe18deefee77e23fcf7c8b13cebc1fc49173b3
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-07 08:18:38 +0300

    updated policies

--- src/components/Policy.js
+++ src/components/Policy.js
@@ -2,7 +2,33 @@ import React from "react";
 
 function Policy() {
     return (
-        <div></div>
+        <div className="policySection">
+            <h2>UNFPA</h2>
+            <p></p>
+            <h2>PRINCIPLES OF UNFPA REAL ESTATE MANAGEMENT POLICY</h2><br/>
+            <h3>Purpose</h3>
+            <p>The purpose of this policy is to equip UNFPA regional offices, sub-regional offices,
+                field offices, and liaison offices (hereafter collectively referred to as “field
+                offices”) with the information and tools necessary in order to carry out their
+               asks in relation to establishing, maintaining, and operating office premises</p>
+            <h3>Country Office Real Estate Management(COREM)</h3>
+            <p>
+            It is required that all field office premises irrespective of funding source, and
+            location be registered in COREM. Office premises, which are provided free
+            of charge by the host government are also required to be registered in
+            COREM. In case there are project offices(s) apart from the main office, those
+            office(s) should also be recorded separately under the heading of ‘secondary
+            office’ in COREM. This policy and procedure document applies to both main
+            field office premises and project office premises also referred to as secondary
+            or sub-field offices
+            </p>
+            <p>
+            All data entry fields must be completed during registration before an office
+            can be accepted into COREM. Incomplete data entry fields or failure to
+            upload all required supporting documentation will result in the registration not
+            being complete.
+            </p>
+        </div>
     );
 }
 

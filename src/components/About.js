000d0e0f976bdba90ff3e2dfe3b66813c89c0322
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-07 19:58:47 +0300

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,15 @@ function About() {
                 <p>Today the Group Operates a Diverse range of businesses including manufacturing floor 
                     and wall tiles, Manufacturing bituminous products and trading, supplying and fitting of 
                     buildings, road construction materials, and investments in Real estate.</p>
+                <p>BuyRentKenya.com is the largest and leading property marketplace in Kenya. 
+                    Founded in 2012 and has since revolutionised the real estate industry in Kenya, 
+                    by connecting buyers and sellers online in a highly convenient way make it a house 
+                    name and around the world.</p>
+                <h4>What do we do?</h4>
+                <p>We connect buyers with sellers and tenants with landlords through a highly user-friendly real estate experience, bringing 
+                    transparency and transforming our market through technology. The extensive properties, new projects and developments advertised offer variety for one when it comes to real estate.</p>
+                <p>We are not estate agents, but a marketplace that offers the best place for property seekers to find details of all advertised properties available to buy, rent or sell in Kenya.</p>
+                <p>Once you found a property you like, call or send your email enquiry through the form. If you need a little more help deciding, you can also read property guides from our local experts.</p>
             </div>
         </div>
     );

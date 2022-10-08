0a15140c28a52bf09b921ab8c3029c50db0d1a25
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-08 12:26:11 +0300

    footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,9 @@ function Footer() {
                     <hr/>
                     <hr/>
                 </div>
-            <div className="container container-flex">
-                <div className="icons">
+            <div className="footer">
+                <div>
+                    <div className="icons">
                     <a href="https://www.linkedin.com/in/ronald-kipchirchir-034983246/" target="_blank">
                         <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt=""/>
                     </a>
@@ -19,15 +20,18 @@ function Footer() {
                     <a href="https://github.com/McRonaah" target="_blank">
                         <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt=""/>
                     </a>
+                    </div>
+                    <div className="icons">
                     <a href="https://twitter.com/RonaahB" target="_blank">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png" alt=""/>
                     </a>
                     <a href="https://www.facebook.com/ronald.beloved/" target="_blank">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJBHbl5XXLerWLdmDKb1vA2tOZP4X4XVKSLb-mvqNBOQ&s" alt=""/>
                     </a>
-                    <a href="#">
+                    <a href="/">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRRFWVOO3wUC0cSJLAKfY4q3ceT24HxPX-ibqhZANxgg&s" alt=""/>
                     </a>
+                    </div>
                 </div>
                 <div className="copyright">
                     <p>All rights reserved &copy;</p>

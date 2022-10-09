2aa41dbf96d8bd3863a287359d1cfab9f1dcc4a5
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-09 15:00:56 +0300

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Router, Route, } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
@@ -6,26 +7,25 @@ import Contact from "./components/Contact";
 import Policy from "./components/Policy";
 import Error from "./components/Error";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/NavBar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
       <div>
       <Navbar />
-         <Switch>
+        <Router>
           <Route path='/' element={<Home/>} />
           <Route path='/components/About' element={<About/>} />
           <Route path='/components/Services' element={<Services/>} />
           <Route path='/components/Contact' element={<Contact/>} />
           <Route path='/components/Policy' element={<Policy/>} />
           <Route path='/components' element={<Error/>} />
-         </Switch>
-      <Footer />
+          </Router>
+        <Footer />
+
       </div>
-     </Router>
   );
 }
 
